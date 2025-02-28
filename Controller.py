f1707fd5e78e10c8db970d98dbdb7aa73f76caba
Ravi Ranjan <151706367+raviranjan-star@users.noreply.github.com>
2025-02-28 09:52:41 +0530

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -4,9 +4,9 @@ comport='COM10'
 board=pyfirmata2.Arduino(comport)
 led_1=board.get_pin('d:8:o')
 led_2=board.get_pin('d:9:o')
-led_3=board.get_pin('d:7:o')
-led_4=board.get_pin('d:6:o')
-led_5=board.get_pin('d:13:o')
+led_3=board.get_pin('d:10:o')
+led_4=board.get_pin('d:11:o')
+led_5=board.get_pin('d:12:o')
 
 def led(fingerUp):
     for i, state in enumerate(fingerUp):
